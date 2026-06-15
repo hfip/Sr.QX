dda0e13d35110ca298a1c38dd2caaed8ca40a188
Abdulluh.X 🇸🇦 <225096852@student.kfu.edu.sa>
2026-06-15 13:10:20 +0300

    Update video_sniffer.js

--- video_sniffer.js
+++ video_sniffer.js
@@ -1,17 +1,13 @@
 /*
-قناص روابط البث (أكوام وفاصل نت)
+قناص روابط البث العربي
 Developer: Telegram: @Abdulluh_X
 */
 
 if ($request && $request.url) {
     var url = $request.url;
-    // تصفية حزم البث الشائعة ميديا وامتدادات
     if (url.indexOf('.m3u8') !== -1 || url.indexOf('.mp4') !== -1 || url.indexOf('stream') !== -1 || url.indexOf('playlist') !== -1 || url.indexOf('/hls/') !== -1) {
         
-        // إشعار فوري على الآيفون
         $notify("تم صيد رابط البث بنجاح! 🎬", "افتح سجل النظام لنسخه", url);
-        
-        // طباعة في السجل المخصص للسكربتات
         console.log("\n====== VIDEO STREAM URL ======\n" + url + "\n==============================\n");
     }
 }
